72e827370b4c11ac181233cc45edc01a5f095b71
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2023-03-22 10:43:54 -0400

    core deliverables

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,43 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plants, setPlants] = useState([])
+  const [search, setSearch] = useState("")
+
+  useEffect(()=>{
+    fetch("http://localhost:6001/plants")
+    .then(response => response.json())
+    .then(data => setPlants(data))
+
+  },[])
+
+  function handleNewPlant(newPlant){
+    //console.log(newPlant)
+    fetch("http://localhost:6001/plants",{
+      method: "POST",
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(newPlant)
+    })
+    .then(response => response.json())
+    .then(data => setPlants([...plants, data]))
+  }
+
+  function onMcSerch(columbus){
+    setSearch(columbus)
+  }
+
+
+  const filteredPlants = plants.filter(plant => plant.name.toLowerCase().includes(search.toLowerCase()))
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm addNewPlant={handleNewPlant}/>
+      <Search mcSerch={onMcSerch}/>
+      <PlantList plants = {filteredPlants}/>
     </main>
   );
 }
